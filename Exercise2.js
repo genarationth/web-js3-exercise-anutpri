a152c6afa62fcea3469ecf1c3e4290aca3ebcf3a
Anut.pri <124768336+anutpri@users.noreply.github.com>
2023-03-14 08:21:41 +0000

    finish exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -11,11 +11,23 @@ Friends("Jo", "J", "Jan");
 const ageCal = (birthYear) => {
     const currentYear = new Date().getFullYear();
     const yourAge = currentYear - birthYear;
-    return console.log(`Your age is ${yourAge}`);
+    return yourAge;
 
 }
-ageCal(1987);
+const age = ageCal(1987);
+console.log(`Your age is ${age}`);
 
 
-// Part3
 
+
+function nameAges(name1, birthYear1, name2, birthYear2, name3, birthYear3) {
+  const age1 = ageCal(birthYear1);
+  const age2 = ageCal(birthYear2);
+  const age3 = ageCal(birthYear3);
+  
+  console.log(`Welcome ${name1}, you are ${age1}.`);
+  console.log(`Welcome ${name2}, you are ${age2}.`);
+  console.log(`Welcome ${name3}, you are ${age3}.`);
+}
+
+nameAges("A", 1995, "B", 1987, "C", 1975);
